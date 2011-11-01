011de0dbd65b8da0da10eb63558981e81065619f
Frank Waterret <waterret@gmail.com>
2011-10-31 20:19:37 -0400

    add two pngs into repository
    	modified:   Main.hs
    	modified:   Makefile
    	new file:   Spectrum.png
    	new file:   Water.png

--- Main.hs
+++ Main.hs
@@ -8,15 +8,17 @@ waterColor x' y' = colorPixel 255 r 130 60 where
     x = fromIntegral x'
     y = fromIntegral y'
 
-main :: IO ()
-main = do
-    let pixelss = replicate 40 $ map (\g -> colorPixel 255 255 g 0) [0..255]
+spectrum :: [[Pixel]]
+spectrum = replicate 40 $ map (\g -> colorPixel 255 255 g 0) [0..255]
                               ++ map (\r -> colorPixel 255 r 255 0) [254,253..1]
                               ++ map (\b -> colorPixel 255 0 255 b) [0..255]
                               ++ map (\g -> colorPixel 255 0 g 255) [254,253..0]
+
+main :: IO ()
+main = do
     let water = map (\x -> map (\y -> waterColor x y) [-256..256]) [-256..256]
     writePNGFromPixelss "Water.png" water
-    image <- imageFromPixelss pixelss
-    writePNGImage "spectrum.png" image
-    pixelss' <- readPixelssFromPNG "spectrum.png"
-    writePNGFromPixelss "spectrum-copy.png" pixelss'
+    image <- imageFromPixelss spectrum
+    writePNGImage "Spectrum.png" image
+    pixelss <- readPixelssFromPNG "Water.png"
+    writePNGFromPixelss "Water-copy.png" pixelss
--- Makefile
+++ Makefile
@@ -6,4 +6,7 @@ haskell-example : Main.hs
 	ghc -o haskell-example -lpng -Wall -O2 --make Main.hs
 
 clean :
-	rm haskell-example example *.o *.hi *.png
+	rm haskell-example example *.o *.hi *-copy.png
+
+clean-png :
+	rm *.png
